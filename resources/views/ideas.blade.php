5b30dd9a00917f658489deb3172094265afa8eec
Ren Consular <rene.consular@gmail.com>
2026-08-05 17:39:08 +0800

    Finished Lesson 8: Databases, Migrations, and Eloquent;

--- resources/views/ideas.blade.php
+++ resources/views/ideas.blade.php
@@ -12,9 +12,15 @@
           <div class="mt-6 flex items-center  gap-x-6">
               <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
           </div>
-    </form>
-
-    <div>
-      
+        </form>
+        @if ($ideas->count()) 
+    <div class = "mt-6 text-white">
+      <h2 class = "font-bold">Your Ideas</h2>
+      <ul class = "mt-6">
+        @foreach ($ideas as $idea)
+           <li class ="text-sm">{{ $idea->description }}</li> 
+        @endforeach
+      </ul>
     </div>
+    @endif
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,41 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Idea;
 
 
 
 
 
 Route::get('/', function () {
-  $ideas = session()->get('ideas', []);
+  $idea = Idea::query()
+    ->when(request('state'), function ($query, $state) {
+      $query->where('state', $state);
+    })
+    ->get();
+
+
 
   return view('ideas', [
-    'ideas' => $ideas
+    'ideas' => $idea
   ]);
 });
 
 Route::post('/ideas', function () {
   $idea = request('idea');
 
-  session()->push('ideas', $idea);
+  Idea::create([
+    'description' => $idea,
+    'state' => 'pending'
+  ]);
+
+  return redirect('/');
+});
+
+Route::get('/delete-ideas', function () {
+  session()->forget('ideas');
 
   return redirect('/');
 });
