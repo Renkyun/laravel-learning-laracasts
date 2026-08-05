5acf0ce46bbdf8402ff2f34128ddde2aad34bdb6
Ren Consular <rene.consular@gmail.com>
2026-08-06 01:43:57 +0800

    Finished Lesson 9: HTTP Requests and REST

--- resources/views/ideas/index.blade.php
+++ resources/views/ideas/index.blade.php
@@ -2,9 +2,9 @@
     <form method ="POST" action="/ideas" >
       @csrf
         <div class="col-span-full">
-            <label for="about" class="block text-sm/6 font-medium text-white">About</label>
+            <label for="description" class="block text-sm/6 font-medium text-white">About</label>
             <div class="mt-2">
-              <textarea id="about" name="idea" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+              <textarea id="description" name="description" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
             </div>
             <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about yourself.</p>
           </div>
@@ -18,7 +18,7 @@
       <h2 class = "font-bold">Your Ideas</h2>
       <ul class = "mt-6">
         @foreach ($ideas as $idea)
-           <li class ="text-sm">{{ $idea->description }}</li> 
+           <a href="/ideas/{{$idea->id}}" class ="text-sm hover:text-blue-500">{{ $idea->description }}</a> <br>
         @endforeach
       </ul>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -8,34 +8,56 @@ use App\Models\Idea;
 
 
 
-
-Route::get('/', function () {
-  $idea = Idea::query()
-    ->when(request('state'), function ($query, $state) {
-      $query->where('state', $state);
-    })
-    ->get();
+//index
+Route::get('/ideas', function () {
+  $idea = Idea::all();
 
 
 
-  return view('ideas', [
+  return view('ideas/index', [
     'ideas' => $idea
   ]);
 });
 
+//show
+Route::get('/ideas/{idea}', function (Idea $idea) {
+  return view('ideas/show', [
+    'idea' => $idea
+  ]);
+});
+
+//edit
+Route::get('/ideas/{idea}/edit', function (Idea $idea) {
+  return view('ideas/edit', [
+    'idea' => $idea
+  ]);
+});
+
+//update
+Route::patch('/ideas/{idea}', function (Idea $idea) {
+  $idea->update([
+    'description' => request('description')
+  ]);
+
+  return redirect("/ideas/{$idea->id}");
+});
+
+
+//store
 Route::post('/ideas', function () {
   $idea = request('idea');
 
   Idea::create([
-    'description' => $idea,
+    'description' => request('description'),
     'state' => 'pending'
   ]);
 
   return redirect('/');
 });
 
-Route::get('/delete-ideas', function () {
-  session()->forget('ideas');
+//destroy
+Route::delete('/ideas/{idea}', function (Idea $idea) {
+  $idea->delete();
 
-  return redirect('/');
+  return redirect('/ideas');
 });
